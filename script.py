ba19217bd769a1e50ffc58400a7ecde83f3c891b
AnnaPaytakov <paytakov.annamurad2003@gmail.com>
2025-08-07 15:28:52 +0500

    modified script

--- script.py
+++ script.py
@@ -1,13 +1,12 @@
 import torch
-from carvekit.api.high import HiInterface  # корректный импорт
+from carvekit.api.high import HiInterface
 
-# Настройка параметров
 interface = HiInterface(
-    object_type="object",        # "object" для общих объектов, "hairs-like" для волос/людей
+    object_type="object",
     batch_size_seg=1,
     batch_size_matting=1,
     device="cuda" if torch.cuda.is_available() else "cpu",
-    seg_mask_size=640,           # 640 для tracer_b7, 320 для u2net/basnet
+    seg_mask_size=640,           # 640 for tracer_b7 model
     matting_mask_size=2048,
     trimap_prob_threshold=231,
     trimap_dilation=30,
@@ -15,10 +14,9 @@ interface = HiInterface(
     fp16=False
 )
 
-# Обработка изображения:
-images = ["input.jpg"]  # или путь к своей картинке
+images = ["input.jpg"] 
 results = interface(images)
 result = results[0]
 result.save("output.png")
 
-print("✅ Фон успешно удалён и сохранён как output.png")
+print("Removed successfully and Saved as output.png")
